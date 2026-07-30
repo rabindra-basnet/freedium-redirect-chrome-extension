429466b73ce51a8f83f038a0b46d933e77056634
rabindra-basnet <rabindra.basnet@example.com>
2026-07-30 15:30:17 +0545

    Revert "Hide button on Medium homepage, only show on article pages"
    This reverts commit 09fe2a342d17051dac93094bb26f49ae74ea480f.

--- content.js
+++ content.js
@@ -2,12 +2,11 @@
   const url = window.location.href;
   const isMediumDomain = /^https:\/\/([\w-]+\.)?medium\.com\//.test(url);
   const isArticle = isMediumDomain && !/\/\/([\w-]+\.)?medium\.com\/?$/.test(url);
-  const isCustomDomain = !isMediumDomain && document.querySelector(
+  const isMediumPage = isArticle || document.querySelector(
     'meta[name="medium"], ' +
     'meta[property="al:android:app_name"][content="Medium"], ' +
     'meta[property="og:site_name"][content="Medium"]'
   );
-  const isMediumPage = isArticle || isCustomDomain;
 
   if (!isMediumPage) return;
 
